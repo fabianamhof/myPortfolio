fc7864b35cf746d8dc3be8b59a444721a3b23b62
Fabian Amhof <amhof.fabian1234@gmail.com>
2024-04-25 13:27:53 +0200

    add sun

--- src/MountainScene.vue
+++ src/MountainScene.vue
@@ -21,16 +21,33 @@ cloudNodes.Cylinder005.visible = false;
 const camera = shallowRef();
 const cloudsModel = shallowRef();
 const mountainModel = shallowRef();
+const sun = shallowRef();
+const sunObject = shallowRef();
 
 const { onLoop } = useRenderLoop();
 
 let mountainAngle = 0;
 let cloudPath = 0;
+let sunPath = 0;
 onLoop(({ delta, elapsed }) => {
   cloudPath += 0.0005
   if (cloudPath > 1) {
     cloudPath = 0;
   }
+
+  sunPath += 0.005
+  if (sunPath > Math.PI * 2) {
+    sunPath = 0
+  }
+
+  const sunX = Math.cos(sunPath) * 20;
+  const sunZ = Math.sin(sunPath) * 20
+
+  sun.value.position.x = sunX
+  sun.value.position.z = sunZ
+  sunObject.value.position.x = sunX
+  sunObject.value.position.y = 20
+  sunObject.value.position.z = sunZ
 
   var docHeight = document.documentElement.scrollHeight;
   var scrollPosition = window.screenY || document.documentElement.scrollTop;
@@ -53,9 +70,13 @@ onMounted(() => {
     <TresCanvas v-bind="gl" style="position: absolute">
       <TresPerspectiveCamera ref="camera" :position="new Vector3(20, 0, 0)" :look-at="new Vector3(0, 10, 0)" />
       <primitive ref="mountainModel" :object="mountain" :position="[0, 0, 0]" />
-      <primitive ref="cloudsModel" :object="clouds" :position="[-40, 20, 0]" :rotation="[0, 1, 0]" :scale="[5, 5, 5]" />
-      <TresDirectionalLight color="white" :position="[10, 10, 0]" :intensity="3" />
-      <TresAmbientLight :intensity="0.4" />
+      <primitive ref="cloudsModel" :object="clouds" :position="[-40, 20, 300]" :rotation="[0, 1, 0]"
+        :scale="[5, 5, 5]" />
+      <TresMesh ref="sunObject" :radius="5" :position="[0, 15, 0]">
+        <TresSphereGeometry></TresSphereGeometry>
+      </TresMesh>
+      <TresDirectionalLight ref="sun" color="#fbe8c9" :position="[5, 10, 200]" :intensity="15" />
+      <TresAmbientLight :intensity="1" />
     </TresCanvas>
   </div>
 </template>
