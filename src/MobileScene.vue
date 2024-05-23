ce2f347724441b8c488a6eb1294f32c2e697ee3f
Fabian Amhof <amhof.fabian1234@gmail.com>
2024-05-23 21:02:32 +0200

    fix pixel ratio

--- src/MobileScene.vue
+++ src/MobileScene.vue
@@ -230,6 +230,7 @@ function updateWidth() {
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
 }
 </script>
 
